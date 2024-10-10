282b1179d0b870e6cd2adbc74d83aa0d0b4d8b3b
ChaJunYeong <34412522+cjy8922@users.noreply.github.com>
2024-10-10 15:53:07 +0900

    Title: 술래잡기, Time: 213ms, Memory: 26MB, Status: Passed - CodeTree

--- 241010/술래잡기/hide-and-seek.py
+++ 241010/술래잡기/hide-and-seek.py
@@ -1,70 +1,71 @@
-N, M, H, K = map(int, input().split())
+N, MM, H, K = map(int, input().split())
 
-# 도망자 초기화 (좌 우 하 상)
-arr = [list(map(int, input().split())) for _ in range(M)]
-di = [ 0, 0, 1, -1]
-dj = [-1, 1, 0,  0]
-opp = {0:1, 1:0, 2:3, 3:2}  # 반대 방향
+# 도망자 좌표 입력
+arr = []
+for _ in range(MM):
+    arr.append(list(map(int, input().split())))
 
-# 나무 좌표 초기화
+# 나무좌표 입력
 tree = set()
 for _ in range(H):
     i,j=map(int, input().split())
     tree.add((i,j))
 
-# 술래의 이동 초기화 (상 우 하 좌)
-tdi = [-1, 0, 1,  0]
-tdj = [ 0, 1, 0, -1]
-max_cnt, cnt, flag, val = 1, 0, 0, 1
-tm = (N + 1) // 2
-ti, tj, td = tm, tm, 0
+# 0(좌) 1(우) 2(하) 3(상)
+di = [ 0, 0, 1,-1]
+dj = [-1, 1, 0, 0]
+opp = {0:1, 1:0, 2:3, 3:2}  # 반대방향
 
-# 게임 시작
+# 방향  상 우 하 좌   tagger(술래)방향 (바깥으로 돌 때 방향)
+tdi = [-1, 0, 1, 0]
+tdj = [ 0, 1, 0,-1]
+
+mx_cnt, cnt, flag, val = 1, 0, 0, 1
+M = (N+1)//2
+ti,tj,td = M,M,0
+
 ans = 0
-for k in range(1, K+1):
-    # 1. 도망자의 이동 (arr)
+for k in range(1,K+1):      # K턴만큼 게임 진행
+    # [1] 도망자의 이동(arr)
     for i in range(len(arr)):
-        if abs(arr[i][0] - ti) + abs(arr[i][1] - tj) <= 3: # 술래와 거리가 3 이하인 경우 이동
-            ni, nj = arr[i][0] + di[arr[i][2]], arr[i][1] + dj[arr[i][2]]
+        if abs(arr[i][0]-ti)+abs(arr[i][1]-tj)<=3:  # 술래와 거리 3이하인 경우 이동
+            ni,nj=arr[i][0]+di[arr[i][2]],arr[i][1]+dj[arr[i][2]]
+            if 1<=ni<=N and 1<=nj<=N:   # 범위내면 술래체크
+                if (ni,nj)!=(ti,tj):    # 술래위치가 아니면 이동
+                    arr[i][0],arr[i][1]=ni,nj
+            else:                       # 범위밖=>방향 반대
+                arr[i][2]=opp[arr[i][2]]# 반대 방향전환 및 저장
+                ni,nj=arr[i][0]+di[arr[i][2]],arr[i][1]+dj[arr[i][2]]
+                if (ni,nj)!=(ti,tj):
+                    arr[i][0],arr[i][1]=ni,nj
 
-            if 1<=ni<=N and 1<=nj<=N: # 이동 방향이 범위 안인 경우 --> 술래 체크 --> 없으면 이동
-                if (ni, nj) != (ti, tj):
-                    arr[i][0], arr[i][1] = ni, nj
-            else:                     # 범위 밖인 경우 방향 반대 --> 술래 체크 --> 없으면 이동
-                nd = opp[arr[i][2]]
-                ni, nj = arr[i][0] + di[nd], arr[i][1] + dj[nd]
-                if (ni, nj) != (ti, tj):
-                    arr[i] = [ni, nj, nd]
-                else:
-                    arr[i][2] = nd
-
-    # 2. 술래의 이동
-    cnt += 1
-    ti, tj = ti + tdi[td], tj + tdj[td]
-    if (ti, tj) == (1, 1):      # 안쪽으로 동작하는 달팽이 형태로 변형
-        max_cnt, cnt, flag, val = N, 1, 1, -1
-        td = 2
-    elif (ti, tj) == (M, M):    # 바깥쪽으로 동작하는 달팽이 형태로 변형
-        max_cnt, cnt, flag, val = 1, 0, 0, 1
-        td = 0
+    # [2] 술래의 이동
+    cnt+=1
+    ti,tj = ti+tdi[td], tj+tdj[td]
+    if (ti,tj)==(1,1):  # 안쪽으로 동작하는 달팽이
+        mx_cnt,cnt,flag,val = N,1,1,-1
+        td=2            # 초기방향은 아래로(하)
+    elif (ti,tj)==(M,M):# 바깥으로 동작하는 달팽이
+        mx_cnt,cnt,flag,val = 1,0,0,1
+        td=0
     else:
-        if cnt == max_cnt:      # 방향을 바꿀 지점에 도달
-            cnt = 0             # 초기화
-            td = (td + val) % 4 # 방향을 바꿈 (상, 우, 하, 좌 순으로)
-            if flag == 0:
-                flag = 1
+        if cnt==mx_cnt:     # 방향 변경
+            cnt=0
+            td = (td+val)%4
+            if flag==0:
+                flag=1
             else:
-                flag = 0
-                max_cnt += val
+                flag=0      # 두 번에 한 번씩 길이 증가
+                mx_cnt+=val
 
-    # 3. 도망자 잡기 --> 술래자리포함 3칸 + 나무가 없는 도망자면 잡힘
-    t_set = set(((ti, tj), (ti + tdi[td], tj + tdj[td]), (ti + tdi[td] * 2, tj + tdj[td] * 2)))
-    for i in range(len(arr) -1, -1, -1):
-        if (arr[i][0], arr[i][1]) in t_set and (arr[i][0], arr[i][1]) not in tree:
+    # [3] 도망자 잡기(술래자리 포함 3칸: 나무가없는 도망자면 잡힘!)
+    tset = set(((ti,tj),(ti+tdi[td],tj+tdj[td]),(ti+tdi[td]*2,tj+tdj[td]*2)))
+    for i in range(len(arr)-1,-1,-1):
+        if (arr[i][0],arr[i][1]) in tset and (arr[i][0],arr[i][1]) not in tree:
             arr.pop(i)
-            ans += k
+            ans+=k
 
-    # 4. 도망자가 없으면 점수도 없음
-    if not arr: break
-
+    # 도망자가 없다면 더이상 점수도 없음
+    if not arr:
+        break
 print(ans)
